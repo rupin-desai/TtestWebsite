dc84b0b315ce5280893dfe2513d13f6fd06a39f0
Rupin Desai <krih382002@gmail.com>
2024-12-15 15:49:02 +0530

    Changes to contact us on Home Page & Contact page

--- Scripts/ContactUsScripts.js
+++ Scripts/ContactUsScripts.js
@@ -7,17 +7,24 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();  // Prevent form submission
 
+    // Show the loader overlay
+    document.getElementById("loader").style.visibility = "visible"; // Make loader visible
+
     // Get the form element
     var formElement = document.getElementById("contactForm");
 
-    // Send email using EmailJS
+    // Send the email using EmailJS
     emailjs.sendForm('service_ejxljdv', 'template_hhi164q', formElement)
     .then(function(response) {
         console.log('Success:', response);
+        // Hide the loader immediately after successful submission
+        document.getElementById("loader").style.visibility = "hidden";
         alert('Your message has been sent successfully!');
     })
     .catch(function(error) {
         console.log('Error:', error);
+        // Hide the loader immediately after error
+        document.getElementById("loader").style.visibility = "hidden";
         alert('There was an error sending your message. Please try again later.');
     });
 });
